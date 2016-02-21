8a7fca4e6a0f104ac4cd458bc71a48e7d74a2486
Justin Hill <jhillkwaj@gmail.com>
2016-02-21 13:17:59 -0600

    New scripts and prefabs

---
+++ JollyGameJam2016/Assets/Destroy_Block.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_Block : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Respawn")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
