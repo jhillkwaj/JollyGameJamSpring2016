97d4525da7cd60ff7a6f36d11a17c66f19cb51b3
Justin Hill <jhillkwaj@gmail.com>
2016-02-20 22:34:54 -0600

    Added portals between levels

---
+++ JollyGameJam2016/Assets/Scripts/Next_Level.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Next_Level : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        
+       Application.LoadLevel(Application.loadedLevel + 1);
+       Debug.Log("Next Level"); 
+     
+    }
+}
