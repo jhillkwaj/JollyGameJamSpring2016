eb4e80422e7fe2bb12a94a4d84a8d88b59642d33
jhillkwaj <jhillkwaj@gmail.com>
2016-02-21 20:32:25 -0600

    Made enter or space navigate through the main menu

---
+++ JollyGameJam2016/Assets/Scripts/Next_Scene_On_Enter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Next_Scene_On_Enter : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+	}
+}
---
+++ JollyGameJam2016/Assets/Scripts/quitGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quitGame : MonoBehaviour
+{
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
